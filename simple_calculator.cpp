ca27f2de8fcd31aaa28633c4a1d63917db846f55
anuragdhiman-696 <anuragdhiman696@gmail.com>
2026-05-17 16:14:02 +0530

    simple_calculator.cpp

--- simple_calculator.cpp
+++ simple_calculator.cpp
@@ -1,52 +1,76 @@
 #include<iostream>
 using namespace std;
 
+float add(float a, float b) {
+    return a + b;
+}
+
+float subtract(float a, float b) {
+    return a - b;
+}
+
+float multiply(float a, float b) {
+    return a * b;
+}
+
+float divide(float a, float b) {
+    return a / b;
+}
+
 int main() {
 
     float firstNumber, secondNumber;
     char operation;
+    char choice;
 
-    cout << "Enter first number: ";
-    cin >> firstNumber;
+    do {
 
-    cout << "Choose operation (+, -, *, /): ";
-    cin >> operation;
+        cout << "\nEnter first number: ";
+        cin >> firstNumber;
 
-    cout << "Enter second number: ";
-    cin >> secondNumber;
+        cout << "Choose operation (+, -, *, /): ";
+        cin >> operation;
 
-    switch(operation) {
+        cout << "Enter second number: ";
+        cin >> secondNumber;
+        switch(operation) {
 
-        case '+':
-            cout << "\nAnswer = " 
-                 << firstNumber + secondNumber;
-            break;
+            case '+':
+                cout << "\nAnswer = "
+                     << add(firstNumber, secondNumber);
+                break;
 
-        case '-':
-            cout << "\nAnswer = " 
-                 << firstNumber - secondNumber;
-            break;
+            case '-':
+                cout << "\nAnswer = "
+                     << subtract(firstNumber, secondNumber);
+                break;
 
-        case '*':
-            cout << "\nAnswer = " 
-                 << firstNumber * secondNumber;
-            break;
+            case '*':
+                cout << "\nAnswer = "
+                     << multiply(firstNumber, secondNumber);
+                break;
 
-        case '/':
+            case '/':
 
-            if(secondNumber == 0) {
-                cout << "\nCannot divide by zero!";
-            }
-            else {
-                cout << "\nAnswer = " 
-                     << firstNumber / secondNumber;
-            }
+                if(secondNumber == 0) {
+                    cout << "\nCannot divide by zero!";
+                }
+                else {
+                    cout << "\nAnswer = "
+                         << divide(firstNumber, secondNumber);
+                }
 
-            break;
+                break;
 
-        default:
-            cout << "\nInvalid operation selected!";
-    }
+            default:
+                cout << "\nInvalid operation selected!";
+        }
+
+        cout << "\n\nDo you want to calculate again? (y/n): ";
+        cin >> choice;
+
+    } while(choice == 'y' || choice == 'Y');
+
 
     return 0;
 }
